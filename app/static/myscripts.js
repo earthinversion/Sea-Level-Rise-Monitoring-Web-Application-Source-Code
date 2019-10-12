3a24b839984de09a6510f45e5bab60aff19501a6
utpalrai <utpalkumar50@gmail.com>
2019-10-12 22:52:52 +0800

    version 0.2

--- app/static/myscripts.js
+++ app/static/myscripts.js
@@ -5,12 +5,12 @@
   });
 
   function showSidebar() {
-    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("mySidebar").style.height = "100%";
     document.getElementById("openbtn").style.display = "None";
     document.getElementById("closebtn").style.display = "block";
 }
   function hideSidebar() {
-    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("mySidebar").style.height = "0";
     document.getElementById("openbtn").style.display = "block";
     document.getElementById("closebtn").style.display = "none";
 }
