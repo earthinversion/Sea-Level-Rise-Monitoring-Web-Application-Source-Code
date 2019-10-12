da8d68a2139ce733fd63cfb681ca7d68c43bbf98
utpalrai <utpalkumar50@gmail.com>
2019-10-12 13:55:53 +0800

    version 0.2

--- app/static/myscripts.js
+++ app/static/myscripts.js
@@ -1,11 +1,16 @@
-  /* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
-  function openNav() {
-      document.getElementById("mySidebar").style.width = "250px";
-      document.getElementById("main").style.marginLeft = "0px";
-    };
-    
-    /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
-    function closeNav() {
-      document.getElementById("mySidebar").style.width = "0";
-      document.getElementById("main").style.marginLeft = "0";
-    };
+ 
+    document.addEventListener('DOMContentLoaded', function() {
+      document.getElementById('openbtn').onclick = showSidebar;
+      document.getElementById('closebtn').onclick = hideSidebar;
+  });
+
+  function showSidebar() {
+    document.getElementById("mySidebar").style.width = "250px";
+    document.getElementById("openbtn").style.display = "None";
+    document.getElementById("closebtn").style.display = "block";
+}
+  function hideSidebar() {
+    document.getElementById("mySidebar").style.width = "0px";
+    document.getElementById("openbtn").style.display = "block";
+    document.getElementById("closebtn").style.display = "none";
+}
